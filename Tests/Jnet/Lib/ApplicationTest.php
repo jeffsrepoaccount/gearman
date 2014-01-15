e4687e7d2dc51d31348d5bca4d8a80eb584e1a62
Jeff Lambert <jefflikeschicken@gmail.com>
2014-01-15 11:07:41 -0500

    Adding phpunit config file, tests directory and testing environment config file for application

---
+++ Tests/Jnet/Lib/ApplicationTest.php
@@ -0,0 +1,12 @@
+<?php namespace Tests;
+
+use Jnet\Lib\Application as Application;
+use \PHPUnit_Framework_TestCase as PHPUnit_Framework_TestCase;
+
+class ApplicationTest extends PHPUnit_Framework_TestCase
+{
+    public function testOne( )
+    {
+        $this->assertTrue( true );
+    }
+}
